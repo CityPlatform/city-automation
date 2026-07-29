b7e24fba918bd0e149551b3849d9f3d1e1448fc9
CityPlatform <ppatel@city-mtg.com>
2026-07-28 23:27:48 -0500

    Add files via upload

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export const SERVICE_NAME = "city-automation";
-export const VERSION = "1.3.0";
-export const BATCH_LIMIT = 8;
-export const CONCURRENCY = 4;
+export const VERSION = "1.4.0";
+export const BATCH_LIMIT = 20;
+export const CONCURRENCY = 6;
 export const RETRY_ATTEMPTS = 1;
 export const RETRY_DELAY_MS = 300;
